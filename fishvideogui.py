22ebb5e48dc7f20a7c01886220d45beba0d21073
henninger <graslandschaft@posteo.de>
2013-09-29 23:42:45 +0200

    initial commit

--- fishvideogui.py
+++ fishvideogui.py
@@ -1,43 +1,54 @@
 #! /usr/bin/env python
-try:
-	from PyQt4 import QtGui, QtCore, Qt
-except Exception, details:
-	print 'Unfortunately, your system misses the PyQt4 packages.'
-	quit()
-try:
-	import wikitools
-except Exception, details:
-	print 'Unfortunately, your system misses wikitools.'
-	quit()
-import os,sys
-
 __author__ = 'henninger'
 
+# #######################################
+
+try:
+    from PyQt4 import QtGui, QtCore, Qt
+except Exception, details:
+    print
+    'Unfortunately, your system misses the PyQt4 packages.'
+    quit()
+
+import os
+import sys
+
+# #######################################
+
 class Main(QtGui.QWidget):
-	def __init__(self, parent=None):
-		QtGui.QWidget.__init__(self, parent)
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self, parent)
 
-		# #######################################
+        # #######################################
 
-		# GEOMETRY
-		width = 600
-		hight = 700
-		offsetLeft = 50
-		offsetTop = 50
+        # GEOMETRY
+        width = 600
+        hight = 700
+        offsetLeft = 50
+        offsetTop = 50
 
-		self.setGeometry(offsetLeft, offsetTop, width, hight)
-		self.setFixedSize(width,hight)
-		self.setWindowTitle('MediaWiki File-Uploader')
+        self.setGeometry(offsetLeft, offsetTop, width, hight)
+        self.setFixedSize(width, hight)
+        self.setWindowTitle('MediaWiki File-Uploader')
 
-		self.mainLayout = QtGui.QGridLayout()
-		self.setLayout( self.mainLayout )
+        self.mainLayout = QtGui.QGridLayout()
+        self.setLayout(self.mainLayout)
 
-		# #######################################
+        # #######################################
 
-		# the config
-		self.cfg = {}
-		self.cfg['cfg_file'] = '.mw_file_uploader.cfg'
-		self.cfg['cfg_options'] = ['wiki_paths', 'users']
+        # the config
+        self.cfg = dict
 
-		# use input arguments
-		self.prog_name = sys.argv[0]
+        # use input arguments
+        self.prog_name = sys.argv[0]
+
+
+# #######################################
+
+if __name__ == "__main__":
+
+    # entering the gui app
+    qapp = QtGui.QApplication(sys.argv)
+    main = Main()
+    main.show()
+    exit(qapp.exec_())
