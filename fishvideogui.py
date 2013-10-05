7e92c1be61376bda2db9311bd7a64c534774110a
henninger <graslandschaft@posteo.de>
2013-10-05 23:04:11 +0200

    Added: - tabs are created and popullated with dictionary of dictionary - added all buttons as schemed

--- fishvideogui.py
+++ fishvideogui.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python
-__author__ = 'henninger'
+__author__ = 'henningerj'
 
 # #######################################
 '''
-Note:
+Notes:
 QMainWindow allows for funky thinks like menu- and tool-bars
 '''
 
 # #######################################
 
-# TODO make list of todos
+# TODO video-canvas: resize with mainwindow
+# TODO Metadata: read-in and show metadata-entries for dictionary of dictionary and connect to source-dictonaries
+# TODO create structure for worker-threads: data-thread, control-thread
 
 # #######################################
 
@@ -28,11 +30,36 @@ from PIL import ImageQt as iqt
 
 # #######################################
 
-#TODO whatever
 
 class Main(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
+
+        # #######################################
+        # ORGANIZATION
+
+        # the config
+        # self.cfg = dict()
+
+        # use input arguments
+        self.prog_name = sys.argv[0]
+
+
+        # some debugging stuff
+        metadata_a = dict()
+        metadata_b = dict()
+
+        metadata_a['A'] = 0
+        metadata_a['B'] = 'stuff'
+        metadata_a['C'] = 'lots of stuff'
+
+        metadata_b['A'] = 1
+        metadata_b['B'] = 'more stuff'
+        metadata_b['C'] = 'really a lot of stuff'
+
+        metadata = dict()
+        metadata['Metadata A'] = metadata_a
+        metadata['Metadata B'] = metadata_b
 
         # #######################################
         # GEOMETRY
@@ -41,6 +68,8 @@ class Main(QtGui.QMainWindow):
         hight = 600
         offset_left = 50
         offset_top = 50
+
+        max_tab_width = 300
 
         self.setGeometry(offset_left, offset_top, width, hight)
         self.setSizePolicy(Qt.QSizePolicy.Maximum, Qt.QSizePolicy.Maximum)
@@ -67,37 +96,38 @@ class Main(QtGui.QMainWindow):
 
         self.video_canvas = QtGui.QLabel(self)
         self.tab = QtGui.QTabWidget()
-        self.page_a = QtGui.QWidget()
-        self.page_b = QtGui.QWidget()
-        self.tab.addTab(self.page_a, "MetaData A")
-        self.tab.addTab(self.page_b, "MetaData B")
+        self.tab.setMaximumWidth(max_tab_width)
 
         self.top_layout.addWidget(self.video_canvas)
         self.top_layout.addWidget(self.tab)
 
+        # populate tab
+        self.pages = dict()
+        for metadata_listname in metadata.keys():
+            self.pages[metadata_listname] = (QtGui.QWidget())
+            self.tab.addTab(self.pages[metadata_listname], metadata_listname)
+
+            self.page_layout = QtGui.QHBoxLayout()
+            self.pages[metadata_listname].setLayout(self.page_layout)
+
+            self.page_scroll = Qt.QScrollArea()
+            self.page_layout.addWidget(self.page_scroll)
+
+            self.page_scroll_contents = QtGui.QWidget()
+
+            self.page_scroll_layout = QtGui.QVBoxLayout(self.page_scroll_contents)
+            self.page_scroll_contents.setLayout(self.page_scroll_layout)
+
+            self.page_scroll.setWidgetResizable(False)
+
+            for entry_name in metadata[metadata_listname].keys():
+                entry = Metadata_Entry(metadata_listname, entry_name, metadata[metadata_listname][entry_name])
+                self.page_scroll_layout.addWidget(entry)
+
+            self.page_scroll.setWidget(self.page_scroll_contents)
+
         # #######################################
-        # tab page a
-        self.page_a_layout = QtGui.QHBoxLayout()
-        self.page_a.setLayout(self.page_a_layout)
-
-        self.page_a_scroll = Qt.QScrollArea()
-        self.page_a_layout.addWidget(self.page_a_scroll)
-
-        self.page_a_scroll_contents = QtGui.QWidget()
-        self.page_a_scroll.setWidget(self.page_a_scroll_contents)
-
-        self.page_a_scroll_layout = QtGui.QVBoxLayout(self.page_a_scroll_contents)
-        self.page_a_scroll_contents.setLayout(self.page_a_scroll_layout)
-
-        self.page_a_scroll.setLayout(self.page_a_scroll_layout)
-
-        self.page_a_scroll.setWidgetResizable(True)
-        for i in xrange(25):
-            label = QtGui.QCheckBox()
-            self.page_a_scroll_layout.addWidget(label)
-
-        # #######################################
-        # canvas
+        # CANVAS
 
         # DEBUG: canvas pixel size
         canvas_v = 800
@@ -112,29 +142,106 @@ class Main(QtGui.QMainWindow):
         # #######################################
         # POPULATE BOTTOM LAYOUT
 
-        self.button_a = QtGui.QPushButton('Button A')
-        self.button_b = QtGui.QPushButton('Button B')
-        self.button_c = QtGui.QPushButton('Button C')
-        self.button_d = QtGui.QPushButton('Button D')
+        self.button_a = QtGui.QPushButton('Start Recording')
+        self.button_b = QtGui.QPushButton('Stop')
+        self.button_c = QtGui.QPushButton('Play')
+        self.button_d = QtGui.QPushButton('Save')
+        self.button_e = QtGui.QPushButton('Discard')
+        self.button_f = QtGui.QPushButton('Comment')
 
         self.button_a.setMinimumHeight(50)
         self.button_b.setMinimumHeight(50)
         self.button_c.setMinimumHeight(50)
         self.button_d.setMinimumHeight(50)
+        self.button_e.setMinimumHeight(50)
+        self.button_f.setMinimumHeight(50)
 
         self.bottom_layout.addWidget(self.button_a)
         self.bottom_layout.addWidget(self.button_b)
         self.bottom_layout.addWidget(self.button_c)
         self.bottom_layout.addWidget(self.button_d)
+        self.bottom_layout.addWidget(self.button_e)
+        self.bottom_layout.addWidget(self.button_f)
+
+        # #######################################
+        # WORKER THREADS
+
+        # #######################################
+        # CONNECTIONS
+
+        # connect buttons
+        self.connect(self.button_a, QtCore.SIGNAL('clicked()'), self.bt_a)
+        self.connect(self.button_b, QtCore.SIGNAL('clicked()'), self.bt_b)
+        self.connect(self.button_c, QtCore.SIGNAL('clicked()'), self.bt_c)
+        self.connect(self.button_d, QtCore.SIGNAL('clicked()'), self.bt_d)
+        self.connect(self.button_e, QtCore.SIGNAL('clicked()'), self.bt_e)
+        self.connect(self.button_f, QtCore.SIGNAL('clicked()'), self.bt_f)
+
+        # create keyboard shortcuts
+        self.create_actions()
 
         # #######################################
 
-        # the config
-        # self.cfg = dict
+    '''
+    Note:
+    Buttons...
+     ... can be made inactive
+     ... have on/off states
+     ... can change text, color etc
+     depending on program state.
+    '''
 
-        # use input arguments
-        self.prog_name = sys.argv[0]
+    # GUI OBJECT CONNECTORS
+    def bt_a(self):
+        pass
 
+    def bt_b(self):
+        pass
+
+    def bt_c(self):
+        pass
+
+    def bt_d(self):
+        pass
+
+    def bt_e(self):
+        pass
+
+    def bt_f(self):
+        pass
+
+    def update_video(self):
+        pass
+
+    # ACTIONS
+    def create_actions(self):
+        # EXAMPLE
+        # Close Program
+        self.action_cancel = QtGui.QAction("Cancel Program", self)
+        self.action_cancel.setShortcut(Qt.Qt.Key_Escape)
+        self.connect(self.action_cancel, QtCore.SIGNAL('triggered()'), qapp, QtCore.SLOT("quit()"))
+        self.addAction(self.action_cancel)
+
+        # FUNCTIONS
+
+# #######################################
+
+
+class Metadata_Entry(QtGui.QWidget):
+    def __init__(self, listname, entryname, entry, parent=None):
+        QtGui.QWidget.__init__(self, parent)
+
+        self.listname = listname
+        self.entryname = entryname
+
+        self.label = QtGui.QLabel(entryname + ':')
+        self.lineedit = QtGui.QLineEdit(str(entry))
+
+        self.layout = QtGui.QHBoxLayout()
+        self.setLayout(self.layout)
+
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.lineedit)
 
 # #######################################
 
