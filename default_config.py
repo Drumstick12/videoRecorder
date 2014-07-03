4d413e64f61eaa34fed25536786acf1faca1b180
henninger <graslandschaft@posteo.de>
2014-07-04 01:07:21 +0200

    added: options parser,\n -programmed stop,\n -frame timestamps,\n -automated start

--- default_config.py
+++ default_config.py
@@ -1,5 +1,5 @@
 # metadata
-default_template = './templates/chripChamber_template.xml'
+default_template = './templates/continuous_recording_template.xml'
 
 # GEOMETRY of mainwindow at start-up
 width, height = 800, 600
@@ -8,7 +8,7 @@ max_tab_width, min_tab_width = 640, 480
 
 
 # camera configurations
-camera_device_search_range = range(0,20)
+camera_device_search_range = range(0, 20)
 camera_name_format = 'camera%02i'
 
 frames_per_second = 25
