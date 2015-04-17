c537c83a1da4b1089f467a4fc581b568e4b21f5e
Lorand Madai-Tahy <lorand.madai@uni-tuebingen.de>
2015-04-17 17:54:35 +0000

    added labels of attributes

--- RasPiCamControllerTab.py
+++ RasPiCamControllerTab.py
@@ -7,4 +7,17 @@ except Exception, details:
 class RasPiCamControllerTab(QtGui.QWidget):
 	def __init__(self, camera):
 		super(RasPiCamControllerTab, self).__init__()
+		
+		self.main_layout = QtGui.QVBoxLayout()
+		self.setLayout(self.main_layout)
+		
+		for att in camera.cam_controller.attributes:
+			new_h_layout = QtGui.QHBoxLayout()
+			label_name = QtGui.QLabel(att.name, self)
+			new_h_layout.addWidget(label_name)
+			
+			# TODO add spinbox/dropdown menu
+			# TODO connect spinbox/dropdown with handle
+			
+			self.main_layout.addLayout(new_h_layout)
 		return
